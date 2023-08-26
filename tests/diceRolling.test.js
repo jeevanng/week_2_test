1d534f691508f95c217b61ea3d4ffa5a6b1ec9e6
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-08-26 10:59:23 +1000

    Added more tests

--- tests/diceRolling.test.js
+++ tests/diceRolling.test.js
@@ -1,11 +1,28 @@
 const{rollDice} = require('../src/diceRolling');
 
+
+beforeEach(() => {
+    console.log("message runs BEFORE each individual test.")
+}); 
+
+afterEach(() => {
+    console.log("message runs AFTER each individual test")
+});
+
+// We can use this in a real life example. Before each test, we connect to a database. 
+// Then after the test, we can disconnect from the database.
+
 describe("Casual TableTop gamer wants to...", () => {
 
    test("...wants to roll a D6 and see a valid result.", () => {
 
-        expect(rollDice().toBeGreaterThan(0));
+        // Calling the function twice, it will not return same number since it is random 
+        // instead we can;
 
+        let diceResult = rollDice();
+
+        expect(diceResult).toBeGreaterThan(0);
+        expect(diceResult).toBeLessThanOrEqual(6);
    }); 
 
 });
@@ -13,31 +30,89 @@ describe("Casual TableTop gamer wants to...", () => {
 describe("Tabletop RPG gamer...", () => {
 
     test("...wants to roll a D20 with advantage and see both rolled results as well as best roll.", () => {
- 
+
     }); 
 
     test("...wants to roll a D20 with disadvantage and see both rolled results as well as worst roll.", () => {
  
     }); 
 
+    test("...wants to roll a D20 one hundred times and see how many natural 20s they get.", () => {
+        let diceResults = [];
+
+        for (let index = 0; index < 100; index++) {
+          diceResults.push(rollDice(20));
+            
+        }
+
+        console.log(diceResults);
+        
+        expect(diceResults).toContain(20);
+        expect(diceResults).toHaveLength(100);
+    });
+
     // In DnD we have these dice;
     // D4, D6, D8, D10, D12, D20, D100
 
     describe("...wants to roll a variety dice, specifically, they want to roll a ...", () => {
 
-        test("D4", () => {
-
+        let diceVarieties = [
+            {
+                diceSize: 4,
+                diceName: "D4"
+            },
+            {
+                diceSize: 6,
+                diceName: "D6"
+            },
+            {
+                diceSize: 8,
+                diceName: "D8"
+            },
+            {
+                diceSize: 10,
+                diceName: "D10"
+            },
+            {
+                diceSize: 12,
+                diceName: "D12"
+            },
+            {
+                diceSize: 20,
+                diceName: "D20"
+            },
+            {
+                diceSize: 100,
+                diceName: "D100"
+            }
+        ]
+        
+        test.each(diceVarieties)("$diceName", (diceObj) => {
+            let diceResult = rollDice(diceObj.diceSize);
+            expect(diceResult).toBeGreaterThan(0);
+            expect(diceResult).toBeLessThanOrEqual(diceObj.diceSize);
         });
+
+
+        // test("D4", () => {
+        //     let diceResult = rollDice(4);
+        //     expect(diceResult).toBeGreaterThan(0);
+        //     expect(diceResult).toBeLessThanOrEqual(4);
+        // });
 
         
-        test("D6", () => {
-
-        });
+        // test("D6", () => {
+        //     let diceResult = rollDice();
+        //     expect(diceResult).toBeGreaterThan(0);
+        //     expect(diceResult).toBeLessThanOrEqual(6);
+        // });
 
         
-        test("D8", () => {
-
-        });
+        // test("D8", () => {
+        //     let diceResult = rollDice(8);
+        //     expect(diceResult).toBeGreaterThan(0);
+        //     expect(diceResult).toBeLessThanOrEqual(8);
+        // });
 
     });
  
